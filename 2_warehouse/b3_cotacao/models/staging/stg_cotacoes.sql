6a54c6d997c026d619b1e9f3aa32ed6ba10803fb
Iasmim Horrana <horranaiasmim@gmail.com>
2025-09-04 21:03:44 -0300

    feat(staging): adiciona modelo stg_cotacoes para cotações no dbt

--- 2_warehouse/b3_cotacao/models/staging/stg_cotacoes.sql
+++ 2_warehouse/b3_cotacao/models/staging/stg_cotacoes.sql
@@ -1,40 +1,52 @@
+{{
+  config(
+    materialized = 'view',
+    tags = ['staging', 'brapi', 'cotacoes']
+  )
+}}
+
 WITH source AS (
   SELECT * FROM {{ source('brapi_raw', 'cotacoes') }}
 ),
 
-renomeado_e_tipado AS (
-
+string_cleaning AS (
   SELECT
-      -- Chave
-      symbol AS ticker,
-      
-      -- Informações da empresa
-      shortName AS nome_curto_empresa,
-
-      -- Métricas de Preço
-      CAST(regularMarketPrice AS {{ dbt.type_float() }}) AS preco_atual,
-      CAST(regularMarketOpen AS {{ dbt.type_float() }}) AS preco_abertura,
-      CAST(regularMarketPreviousClose AS {{ dbt.type_float() }}) AS preco_fechamento_anterior,
-      CAST(regularMarketDayHigh AS {{ dbt.type_float() }}) AS preco_maximo_dia,
-      CAST(regularMarketDayLow AS {{ dbt.type_float() }}) AS preco_minimo_dia,
-      
-      -- Métricas de Volume e Variação
-      CAST(regularMarketVolume AS {{ dbt.type_float() }}) AS volume_negociado_dia,
-      CAST(regularMarketChange AS {{ dbt.type_float() }}) AS variacao_diaria_valor,
-      CAST(regularMarketChangePercent AS {{ dbt.type_float() }}) AS variacao_diaria_percentual,
-      
-      -- Métricas de Mercado
-      CAST(marketCap AS {{ dbt.type_float() }}) AS valor_de_mercado,
-      CAST(priceEarnings AS {{ dbt.type_float() }}) AS preco_lucro,
-      CAST(earningsPerShare AS {{ dbt.type_float() }}) AS lucro_por_acao,
-
-      -- Informações de Data e Coleta
-      regularMarketTime AS horario_cotacao,
-      data_coleta AS data_coleta_etl
-
+    -- Remove sufixos e espaços extras do nome da empresa
+    TRIM(REGEXP_REPLACE(shortName, 'PN|ON|NM|EDJ|DR2|N1|PN|ON|N2|SA|EJ', '', 'gi')) AS nome_empresa_limpo,
+    *
   FROM source
+),
 
+renomeado_e_tipado AS (
+  SELECT
+    -- Chave
+    symbol AS ticker,
+    
+    -- Informações da empresa
+    nome_empresa_limpo AS nome_empresa, 
+
+    -- Métricas de Preço
+    CAST(regularMarketPrice AS {{ dbt.type_float() }}) AS preco_atual,
+    CAST(regularMarketOpen AS {{ dbt.type_float() }}) AS preco_abertura,
+    CAST(regularMarketPreviousClose AS {{ dbt.type_float() }}) AS preco_fechamento_anterior,
+    CAST(regularMarketDayHigh AS {{ dbt.type_float() }}) AS preco_maximo_dia,
+    CAST(regularMarketDayLow AS {{ dbt.type_float() }}) AS preco_minimo_dia,
+    
+    -- Métricas de Volume e Variação
+    CAST(regularMarketVolume AS {{ dbt.type_float() }}) AS volume_negociado_dia,
+    CAST(regularMarketChange AS {{ dbt.type_float() }}) AS variacao_diaria_valor,
+    CAST(regularMarketChangePercent AS {{ dbt.type_float() }}) AS variacao_diaria_percentual,
+    
+    -- Métricas de Mercado
+    CAST(marketCap AS {{ dbt.type_float() }}) AS valor_de_mercado,
+    CAST(priceEarnings AS {{ dbt.type_float() }}) AS preco_lucro,
+    CAST(earningsPerShare AS {{ dbt.type_float() }}) AS lucro_por_acao,
+
+    -- Informações de Data e Coleta
+    CAST(regularMarketTime AS {{ dbt.type_timestamp() }}) AS horario_cotacao,
+    CAST(data_coleta AS {{ dbt.type_timestamp() }}) AS data_coleta_etl
+  
+  FROM string_cleaning 
 )
 
 SELECT * FROM renomeado_e_tipado
-
